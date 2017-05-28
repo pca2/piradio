ced2935bde16873b6fcbe80780818d2bf3fcdb50
pca2 <atwaterc@gmail.com>
2017-05-28 16:24:31 -0400

    update pin on buradio and add mono option

--- butradio.py
+++ butradio.py
@@ -3,10 +3,10 @@ import time
 import subprocess
 GPIO.setmode(GPIO.BCM)
 
-GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(17, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 while True:
-    input_state = GPIO.input(18)
+    input_state = GPIO.input(17)
     if input_state == False:
-      player = subprocess.Popen(["mpg123", "http://wbur-sc.streamguys.com/wbur.mp3"], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+      player = subprocess.Popen(["mpg123","-m" ,"http://wbur-sc.streamguys.com/wbur.mp3"], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
